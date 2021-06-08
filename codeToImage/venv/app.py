68ddd7ddf848fed9499f09c0e6ac24419da04570
KatoShoma <shoma-1017@i.softbank.jp>
2021-06-08 23:37:38 +0900

    topページへのアクセスを追加

--- codeToImage/venv/app.py
+++ codeToImage/venv/app.py
@@ -4,8 +4,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")#url
-def hello_world():
+def regist():
     return render_template("index.html")
+
+@app.route("/top")
+def top():
+    return render_template("top.html")
 
 if __name__ == '__main__':
     app.run(debug=True,  host='0.0.0.0', port=1017) # ポートの変更
